d98bd4a699a435381c5e2cc27124c693d5fd7582
c.toffolon <c.toffolon@AXCENTNB419>
2022-11-07 15:47:25 +0100

    c

--- telnetProject/src/main/java/com/telnet/telnetProject/TestTelnetNew.java
+++ telnetProject/src/main/java/com/telnet/telnetProject/TestTelnetNew.java
@@ -18,7 +18,7 @@ class TestTelnetNew extends TimerTask
     	String porta = null;
         try
         {
-       	    //Controllare il codice forse perchè corretto
+       	    //Controllare il codice forse perchè corretto!
         	System.out.println("Connessione con : "+ indirizzo);
         	int posi = ((String) indirizzo).indexOf(":") + 1;
         	porta = indirizzo.substring(posi,indirizzo.length()); // 
